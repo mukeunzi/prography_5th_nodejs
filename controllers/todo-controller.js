5138382a2aa1c1ab36f837cb228641f06290317a
mukeunzi <eunji057@gmail.com>
2019-09-03 17:59:50 +0900

    feat : createTodo() 메소드 생성
    - req.body 에서 입력한 데이터를 todoData 에 전송하는 메소드

--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -4,6 +4,12 @@ class TodoController {
 	getTodoList(req, res, next) {
 		res.json(todoData.getTodoList());
 	}
+
+	createTodo(req, res, next) {
+		const todoContents = req.body.addContents;
+		todoData.createTodo(todoContents);
+		res.json(todoData.getTodoList());
+	}
 }
 
 module.exports = new TodoController();
