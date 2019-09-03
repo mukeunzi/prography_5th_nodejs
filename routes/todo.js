cd33709003e1fed272d653deb7e104ded96076b2
mukeunzi <eunji057@gmail.com>
2019-09-03 17:55:12 +0900

    feat : POST '/todo' api 생성

--- routes/todo.js
+++ routes/todo.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const todoController = require('../controllers/todo-controller');
 
 router.get('/', todoController.getTodoList);
+router.post('/', todoController.createTodo);
 
 module.exports = router;
