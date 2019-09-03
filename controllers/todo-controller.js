2b7e1113ab6502778af5a2fc5c35a86c39f71027
mukeunzi <eunji057@gmail.com>
2019-09-04 01:57:48 +0900

    refactor : 변수명 변경
    - todoData => todoDB 로 변경

--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -1,29 +1,32 @@
-const todoData = require('../db/todo');
+const todoDB = require('../db/todo');
 const { makeUniqueId } = require('../utils/unique-id');
 
 class TodoController {
 	getTodoList(req, res, next) {
-		res.json(todoData.getTodoList());
+		res.json(todoDB.getTodoList());
 	}
 
 	createTodo(req, res, next) {
 		const todo_contents = req.body.todo_contents;
 		const newTodoData = { todo_id: makeUniqueId(), todo_contents, status_code: 1 };
-		todoData.createTodo(newTodoData);
-		res.json(todoData.getTodoList());
+
+		todoDB.createTodo(newTodoData);
+		res.json(todoDB.getTodoList());
 	}
 
 	deleteTodo(req, res, next) {
 		const todo_id = req.body.todo_id;
-		todoData.deleteTodo(todo_id);
-		res.json(todoData.getTodoList());
+
+		todoDB.deleteTodo(todo_id);
+		res.json(todoDB.getTodoList());
 	}
 
 	updateTodo(req, res, next) {
 		const { todo_id, todo_contents } = req.body;
 		const updateData = { todo_id, todo_contents };
-		todoData.updateTodo(updateData);
-		res.json(todoData.getTodoList());
+
+		todoDB.updateTodo(updateData);
+		res.json(todoDB.getTodoList());
 	}
 }
 
