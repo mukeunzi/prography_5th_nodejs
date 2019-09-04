15530f3b36c96618c05d9290ea442c86a64d4a69
mukeunzi <eunji057@gmail.com>
2019-09-04 18:45:22 +0900

    fix : PATCH '/todo/:todo_id' api 변경

--- routes/todo.js
+++ routes/todo.js
@@ -5,6 +5,6 @@ const todoController = require('../controllers/todo-controller');
 router.get('/', todoController.getTodoList);
 router.post('/', todoController.createTodo);
 router.delete('/:todo_id', todoController.deleteTodo);
-router.patch('/', todoController.updateTodo);
+router.patch('/:todo_id', todoController.updateTodo);
 
 module.exports = router;
